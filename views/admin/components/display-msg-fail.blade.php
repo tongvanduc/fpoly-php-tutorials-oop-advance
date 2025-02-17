a1fa2333e640e620521d5a2f4046ac7ccb4660b7
Tống Văn Đức <tongduc315@gmail.com>
2025-02-17 10:00:15 +0700

    Fix show message

--- views/admin/components/display-msg-fail.blade.php
+++ views/admin/components/display-msg-fail.blade.php
@@ -1,4 +1,4 @@
-@if (!empty($_SESSION['status']) && !$_SESSION['status'])
+@if (isset($_SESSION['status']) && !$_SESSION['status'])
     <div class="alert alert-danger">
         {{ $_SESSION['msg'] }}
     </div>
